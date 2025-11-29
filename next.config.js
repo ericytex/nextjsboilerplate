3137856a602ca30ea5924305cff651c64016e397
Eric Ytex <ewatyeks@gmail.com>
2025-11-29 16:16:12 +0300

    Fix webpack config: remove custom splitChunks that was breaking chunk generation, add ESLint config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Increase webpack chunk loading timeout
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          cacheGroups: {
-            default: false,
-            vendors: false,
-          },
-        },
-      }
-    }
-    return config
-  },
   // Improve chunk loading reliability
   experimental: {
     optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
